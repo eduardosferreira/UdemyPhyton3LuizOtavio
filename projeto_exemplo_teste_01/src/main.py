025be9acca25a8a4a2047625a97da8cbc4211f79
Eduardo da Silva Ferreira <eduardo.s.ferreira@gmail.com>
2022-02-06 17:48:24 -0300

    inicial

---
+++ projeto_exemplo_teste_01/src/main.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+------------------------------------------------------------------------------
+MODULO ...: <<MODULO>>
+SCRIPT ...: main.py
+CRIACAO ..: DD/MM/YYYY
+AUTOR ....: EDUARDO DA SILVA FERREIRA
+            / KYROS TECNOLOGIA (eduardof@kyros.com.br)
+DESCRICAO.: Este script .....
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+  HISTORICO :
+  DD/MM/YYYY : EDUARDO DA SILVA FERREIRA
+            / KYROS TECNOLOGIA
+            (eduardof@kyros.com.br)
+        - Criação do Script.
+------------------------------------------------------------------------------
+"""
+import os
+import sys
+# sys.path.insert(0, '..')
+
+
+def __fnc_load():
+    """
+        Funcao de carregamento
+    """
+    os.gv_cc_sep_dir = ('/' if os.name == 'posix' else '\\')
+    os.gv_cc_path = ""
+    for index, dir in \
+            enumerate(os.path.dirname(__file__).split(os.gv_cc_sep_dir)):
+        if index == 0:
+            os.gv_cc_path += dir+os.gv_cc_sep_dir
+            continue
+        if dir.upper().strip() == "SRC":
+            sys.path.append(os.gv_cc_path)
+            break
+        os.gv_cc_path = os.path.join(os.gv_cc_path, dir)
+
+
+def main(*args, **kwargs):
+    """
+        Funcao principal
+    """
+    print('sep_dir:', os.gv_cc_sep_dir)
+    print('path:', os.gv_cc_path)
+    print('file:', __file__)
+    print('basename:', os.path.basename(__file__))
+    print('dirname:', os.path.dirname(__file__))
+    print('abspath:', os.path.abspath(__file__))
+    print('abs dirname:', os.path.dirname(os.path.abspath(__file__)))
+
+
+def fnc_teste_01(p_vl_x: float, p_vl_y: float) -> float:
+    """
+        Funcao de teste
+    """
+    return p_vl_x + p_vl_y
+
+
+# Carrega os dados principais
+__fnc_load()
+
+# Processa os dados quando acionado
+if __name__ == '__main__':
+    main()
